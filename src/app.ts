3db11c53dc7fa674305facd90d173f5ea01bb223
cuicoms <cuicoms@iastate.edu>
2023-02-12 14:31:55 -0600

    project env setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Express } from 'express';
+import { ChattyServer } from '@root/setupServer';
+import databaseConnection from '@root/setupDatabase';
+import { config } from '@root/config';
+
+class Application {
+  public initialize(): void {
+    this.loadConfig();
+    databaseConnection();
+    const app: Express = express();
+    const server: ChattyServer = new ChattyServer(app);
+    server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
+    config.cloudinaryConfig();
+  }
+}
+
+const application: Application = new Application();
+application.initialize();
